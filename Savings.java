f102a02e8f3c3c30df376266b2e019537af5bdb5
anayaalex2019 <40498454+anayaalex2019@users.noreply.github.com>
2019-03-21 10:24:59 -0700

    Delete Savings.java

--- Savings.java
+++
@@ -1,9 +0,0 @@
-public class Savings extends Account{
-
-    Savings(int newCustomer, double newBalance) {
-        int customer = newCustomer;
-        double balance = newBalance;
-        System.out.printf("Account %d created.", customer);
-    }
-
-}
