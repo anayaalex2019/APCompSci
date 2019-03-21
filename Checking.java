4a5fb3bb314696ba9b577faf344cbbeaf53a6d05
anayaalex2019 <40498454+anayaalex2019@users.noreply.github.com>
2019-03-21 10:24:25 -0700

    Delete Checking.java

--- Checking.java
+++
@@ -1,9 +0,0 @@
-public class Checking extends Account{
-
-    Checking(int newCustomer, double newBalance) {
-        int customer = newCustomer;
-        double balance = newBalance;
-        System.out.printf("Account %d created.", customer);
-    }
-
-}
